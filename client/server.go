23a76d20702d02f4280511699b8d44760064f5be
iBug <git@ibugone.com>
2022-08-25 02:37:57 +0800

    Accept client-supplied ID and fix lobby slots
    Also try to reduce indentation by changing break logic

--- client/server.go
+++ client/server.go
@@ -11,6 +11,7 @@ import (
 	squares "github.com/iBug/Squares-go"
 )
 
+// Use as "connection control block"
 type ClientInfo struct {
 	id   int
 	conn net.Conn
@@ -32,17 +33,27 @@ var (
 	r1 = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
-func GenerateClientID() int {
+func generateClientID() int {
 	return r1.Intn(IDRANGE) + 1
 }
 
-func FindClientInfo(ci *ClientInfo) int {
+func findClientInfo(ci *ClientInfo) int {
 	for i := range lobby {
 		if lobby[i] == ci {
 			return i
 		}
 	}
 	return -1
+}
+
+func getAvailableLobbySlot() int {
+	for i := range lobby {
+		if lobby[i] == nil {
+			return i
+		}
+	}
+	lobby = append(lobby, nil)
+	return len(lobby) - 1
 }
 
 func handleClient(ci *ClientInfo, ch chan<- ClientMessage) {
@@ -65,88 +76,101 @@ func handleClient(ci *ClientInfo, ch chan<- ClientMessage) {
 	}
 }
 
-func serverGame(chCM <-chan ClientMessage) {
-	// GameLoop:
-	for cm := range chCM {
-		ci := cm.ci
-		num := FindClientInfo(ci)
-	SwitchCMType:
-		switch req := cm.m.(type) {
-		case ConnectReq:
-			if ci.id == 0 && req.Id != 0 {
-				ci.id = req.Id
-			}
-			if num == -1 {
-				if gameOngoing {
-					// Handle potential reconnection
-					for i := range lobby {
-						if req.Id == lobby[i].id {
-							log.Printf("Client[%d] %d reconnected as %s (was %s)\n",
-								i, ci.id, ci.conn.RemoteAddr(), lobby[i].conn.RemoteAddr())
-							lobby[i].conn.Close()
-							lobby[i] = ci
-							SendMsg(ci.conn, ConnectRes{ci.id, i, *game})
-							break SwitchCMType
-						}
-					}
-					// Unrecognized connection
-					log.Printf("Client[?] %d connected while game ongoing\n", ci.id)
-					SendMsg(ci.conn, ServerRes{S_UNKNOWN})
-					ci.conn.Close()
-				} else {
-					// New connection as join request
-					ci.id = GenerateClientID()
-					num = len(lobby)
-					lobby = append(lobby, ci)
-					if len(lobby) == squares.NPLAYERS {
-						// Start game
-						gameOngoing = true
-						game.Reset()
-					}
-					SendMsg(ci.conn, ConnectRes{ci.id, num, *game})
-				}
-			} else {
-				// Existing connection as ping
-				SendMsg(ci.conn, ConnectRes{ci.id, num, *game})
-			}
-		case MoveReq:
-			if !gameOngoing {
-				SendMsg(ci.conn, ServerRes{S_GAME_NOT_GOING})
-				break
-			}
-			if num == game.ActivePlayer {
-				pos := squares.Coord{req.Pos[0], req.Pos[1]}
-				if !game.TryInsert(req.ShapeId, req.Rotation, pos, num, game.FirstRound) {
-					SendMsg(ci.conn, MoveRes{
-						Ok:           false,
-						ActivePlayer: game.ActivePlayer,
-					})
-					break
-				}
-				game.Insert(req.ShapeId, req.Rotation, pos, num)
-				game.AfterMove()
-				for i := 0; i < squares.NPLAYERS; i++ {
-					SendMsg(lobby[i].conn, OtherMoveRes{
-						PlayerId:     num,
-						ShapeId:      req.ShapeId,
-						Pos:          req.Pos,
-						Rotation:     req.Rotation,
-						ActivePlayer: game.ActivePlayer,
-					})
-				}
-			}
-		case ClientDisconnect:
-			if num != -1 {
-				if gameOngoing {
-					log.Printf("Client[%d] %d disconnected while game ongoing", num, ci.id)
-					// just wait for reconnection
-				} else {
-					lobby = append(lobby[:num], lobby[num+1:]...)
-				}
-			}
-		default:
-			log.Printf("Unknown message type: %T\n", req)
+func processClientMessage(cm ClientMessage) {
+	ci := cm.ci
+	num := findClientInfo(ci)
+SwitchCMType:
+	switch req := cm.m.(type) {
+	case ConnectReq:
+		if ci.id == 0 && req.Id != 0 {
+			// Register ID with connection control block
+			ci.id = req.Id
 		}
+		if num != -1 {
+			// Existing connection as ping
+			SendMsg(ci.conn, ConnectRes{ci.id, num, *game})
+			break
+		}
+
+		if gameOngoing {
+			// Handle potential reconnection
+			for i := range lobby {
+				if req.Id == lobby[i].id {
+					log.Printf("Client[%d] %d reconnected as %s (was %s)\n",
+						i, ci.id, ci.conn.RemoteAddr(), lobby[i].conn.RemoteAddr())
+					lobby[i].conn.Close()
+					lobby[i] = ci
+					SendMsg(ci.conn, ConnectRes{ci.id, i, *game})
+					break SwitchCMType
+				}
+			}
+			// Unrecognized connection
+			log.Printf("Client[?] %d connected while game ongoing\n", ci.id)
+			SendMsg(ci.conn, ServerRes{S_UNKNOWN})
+			ci.conn.Close()
+		} else {
+			// New connection as join request
+			ci.id = req.Id
+			if ci.id == 0 {
+				ci.id = generateClientID()
+			}
+			num = len(lobby)
+			slot := getAvailableLobbySlot()
+			lobby[slot] = ci
+			if len(lobby) == squares.NPLAYERS {
+				// Start game
+				gameOngoing = true
+				game.Reset()
+			}
+			SendMsg(ci.conn, ConnectRes{ci.id, num, *game})
+		}
+	case MoveReq:
+		if !gameOngoing {
+			SendMsg(ci.conn, ServerRes{S_GAME_NOT_GOING})
+			break
+		}
+		if num != game.ActivePlayer {
+			log.Printf("Received move from inactive player %d\n", num)
+			break
+		}
+
+		pos := squares.Coord{req.Pos[0], req.Pos[1]}
+		if !game.TryInsert(req.ShapeId, req.Rotation, pos, num, game.FirstRound) {
+			SendMsg(ci.conn, MoveRes{
+				Ok:           false,
+				ActivePlayer: game.ActivePlayer,
+			})
+			break
+		}
+		game.Insert(req.ShapeId, req.Rotation, pos, num)
+		game.AfterMove()
+		for i := 0; i < squares.NPLAYERS; i++ {
+			SendMsg(lobby[i].conn, OtherMoveRes{
+				PlayerId:     num,
+				ShapeId:      req.ShapeId,
+				Pos:          req.Pos,
+				Rotation:     req.Rotation,
+				ActivePlayer: game.ActivePlayer,
+			})
+		}
+	case ClientDisconnect:
+		if num == -1 {
+			break
+		}
+		if gameOngoing {
+			log.Printf("Client[%d] %d disconnected while game ongoing", num, ci.id)
+			// just wait for reconnection
+		} else {
+			lobby[num] = nil
+		}
+	default:
+		log.Printf("Unknown message type: %T\n", req)
+	}
+}
+
+func serverGame(chCM <-chan ClientMessage) {
+	for {
+		processClientMessage(<-chCM)
 	}
 }
 
